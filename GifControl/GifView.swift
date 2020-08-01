75d526d9a7d3fb7f88dc129d68c0a73e3e88470a
Bryan <bryan.wang@berkeley.edu>
2020-08-01 14:48:24 -0700

    got gif to load

---
+++ GifControl/GifView.swift
@@ -0,0 +1,31 @@
+//
+//  GifView.swift
+//  GifControl
+//
+//  Created by Bryan Wang on 8/1/20.
+//  Copyright © 2020 Bryan Wang. All rights reserved.
+//
+
+import SwiftUI
+import Gifu
+
+struct GifView: UIViewRepresentable {
+    func makeUIView(context: Context) -> GIFImageView {
+        let imageView = GIFImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        // sizing doesn't work
+//        imageView.prepareForAnimation(withGIFNamed: "mushroom") {
+//          print("Ready to animate!")
+//        }
+        return imageView
+    }
+    
+    func updateUIView(_ uiView: GIFImageView, context: Context) {
+        uiView.animate(withGIFNamed: "mushroom")
+    }
+}
+
+struct GifView_Previews: PreviewProvider {
+    static var previews: some View {
+        GifView()
+    }
+}
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '12.0'
+
+target 'GifControl' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for GifControl
+  pod 'TinyConstraints'
+  pod 'Gifu'
+
+  target 'GifControlTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'GifControlUITests' do
+    # Pods for testing
+  end
+
+end
